d33c06fab5afb01a6a149dcd4d87620a5a21b658
anastasiazalesova <zalesova98@mail.ru>
2017-05-03 15:34:05 +0300

    Add new form

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170503113837) do
+ActiveRecord::Schema.define(version: 20170503121118) do
 
   create_table "brands", force: :cascade do |t|
     t.string   "name"
     t.string   "logo"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "sneakers", force: :cascade do |t|
+    t.integer  "brand_id"
+    t.string   "sex"
+    t.string   "model"
+    t.integer  "price"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
